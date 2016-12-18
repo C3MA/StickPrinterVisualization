6736a8712fe62408bee403adc89c34e62e603c00
C3MA Werkstatt <werkstatt@lan>
2016-12-18 18:24:00 +0100

    Less memory is lost

--- printerStatus.lua
+++ printerStatus.lua
@@ -1,24 +1,29 @@
 -- Retrive the status of the 3D Printer
 
 requestRunning=false
+srvrconn=nil
 function get3DprinterStatus()
-srvrconn=net.createConnection(net.TCP, 0) 
-    srvrconn:on("receive", function(srvrconn, pl) 
-    --print(pl)
-    found=string.match(pl, "completion\": %d+")
-    if (found ~= nil) then
-        percent=string.match(found, "%d+")
-        print(percent .. "%")
-        numLeds=percent*60/100
-        ws2812.writergb(4, string.char(0,0,30):rep(numLeds) .. string.char(0,0,0):rep(60) )
-    else
-        print("No valid answer")
-    end
-    requestRunning=false
+    srvrconn=net.createConnection(net.TCP, 0) 
+    srvrconn:on("receive", function(con, pl) 
+        --print(pl)
+        local found=string.match(pl, "completion\": %d+")
+        if (found ~= nil) then
+            local percent=string.match(found, "%d+")
+            print(percent .. "% " .. node.heap().. " bytes free")
+            local numLeds=percent*60/100
+            ws2812.writergb(4, string.char(0,0,30):rep(numLeds) .. string.char(0,0,0):rep(60) )
+        else
+            print("No valid answer")
+        end
+        requestRunning=false
+        pl=nil
+        srvrconn:close()
+        collectgarbage()
     end)
     srvrconn:connect(80, printer3D)
     srvrconn:send("GET /api/job?apikey=" .. apikey .. " HTTP/1.1\r\n".."Accept: */*\r\n\r\n")
     requestRunning=true
+    print("Starting a request")
  end
 print("Asking the printer") 
 get3DprinterStatus()
